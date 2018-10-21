9d5e6eaa80e23e905802789d4d81da709b54e374
Ivan Dashkevich <idashkevich@lement.pro>
2018-10-21 17:38:08 +0300

    PNG оптимизированы

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ const gulp = require('gulp'),
     imagemin = require('gulp-imagemin');
 
 gulp.task('images', () => {
-    return gulp.src('./src/assets/*.+(svg)')
+    return gulp.src('./src/assets/*.+(svg|png)')
         .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 5}),
             imagemin.svgo({
                 plugins: [
                     {removeViewBox: true},
